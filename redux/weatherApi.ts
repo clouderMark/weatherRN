760066f8ff48be3216595983b8c20698324c78a6
clouderMark <charnysheuski@yandex.by>
2023-11-09 15:19:09 +0300

    feat: here i try screw redux toolkit. Even dont now need me this thing or no. Just for experience

---
+++ redux/weatherApi.ts
@@ -0,0 +1,30 @@
+import {createApi, fetchBaseQuery, BaseQueryFn, FetchArgs} from '@reduxjs/toolkit/query/react';
+import {TLocation, Weather} from '../types/types';
+
+const WEATHER_API = 'https://api.openweathermap.org/data/2.5';
+const API_KEY = '291ac51ddab2b5ccb5165b5619b5c1eb';
+
+export interface ICustomError {
+  data: {
+    message: string;
+  };
+  status: number;
+}
+
+export const weatherApi = createApi({
+  reducerPath: 'weatherApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: `${WEATHER_API}`,
+    credentials: 'include',
+  }) as BaseQueryFn<string | FetchArgs, unknown, ICustomError, {}>,
+  endpoints: (builder) => ({
+    getWeather: builder.mutation<Weather, TLocation>({
+      query: (location) => ({
+        url: `weather?lat=${location.latitude}&lon=${location.longitude}&appid=${API_KEY}`,
+        method: 'GET',
+      }),
+    }),
+  }),
+});
+
+export const {useGetWeatherMutation} = weatherApi;
