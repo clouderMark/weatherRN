369af75d71e4dc0ce6b91da853480af1b275ff92
clouderMark <charnysheuski@yandex.by>
2023-11-10 14:27:16 +0300

    feat: that is of course intersting thing what in docs wrot what me no need use react-native-flipper like dep. May be I here do things wich is strange and them will be need make in some different way, but now these all looks like normal things

--- redux/store.ts
+++ redux/store.ts
@@ -1,12 +1,19 @@
-import {configureStore} from '@reduxjs/toolkit';
+import {Middleware, configureStore} from '@reduxjs/toolkit';
 import {weatherApi} from './weatherApi';
+
+const middlewares: Middleware[] = [];
+
+if (__DEV__) {
+  const createDebugger = require('redux-flipper').default; // eslint-disable-line
+
+  middlewares.push(createDebugger());
+}
 
 export const store = configureStore({
   reducer: {
     [weatherApi.reducerPath]: weatherApi.reducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(weatherApi.middleware),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(middlewares).concat(weatherApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
