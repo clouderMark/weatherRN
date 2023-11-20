61ff0a9206efb0b5f06b15dd8bcbab975f9865fc
clouderMark <charnysheuski@yandex.by>
2023-11-20 14:12:42 +0300

    refactor: divide component for list item and parent of it

--- src/components/weatherForTheDay/WeatherForTheDay.tsx
+++ src/components/weatherForTheDay/WeatherForTheDay.tsx
@@ -1,16 +1,10 @@
-import {Image, StyleSheet, Text, View, FlatList} from 'react-native';
+import {StyleSheet, View, FlatList} from 'react-native';
 import {selectWetherData} from '../../redux/weatherApi';
 import {useAppSelector} from '../../redux/hooks';
-import {selectLocale} from '../../redux/systemLocale';
-import {selectMode} from '../../redux/colorSchemeSlice';
-import {getImage} from './getImage';
-import {getTextColorForMode} from '../getTextColorForMode';
+import WeatherForHours from './WeatherForHours';
 
 const WeatherForTheDay = () => {
   const {list} = useAppSelector(selectWetherData);
-  const {locale} = useAppSelector(selectLocale);
-  const isDarkMode = useAppSelector(selectMode);
-  const textColor = getTextColorForMode(isDarkMode);
 
   return (
     <View style={styles.container}>
@@ -20,13 +14,7 @@ const WeatherForTheDay = () => {
           data={list}
           horizontal
           renderItem={({item, index}) => (
-            <View style={styles.item}>
-              <Text style={textColor}>
-                {index === 0 ? (locale === 'ru' ? 'Сейчас' : 'now') : new Date(item.dt_txt).getHours()}
-              </Text>
-              <Image style={styles.image} source={getImage(item.weather[0].description, locale, item.dt_txt)} />
-              <Text style={textColor}>{item.main.temp.toFixed()}</Text>
-            </View>
+            <WeatherForHours dt={item.dt} isFirst={!index}/>
           )}
         />
       ) : null}
@@ -44,17 +32,6 @@ const styles = StyleSheet.create({
     paddingTop: 10,
     paddingBottom: 10,
     paddingLeft: 10,
-  },
-  item: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    width: 60,
-  },
-  image: {
-    // flexGrow: 1,
-    width: 42,
-    height: 32,
   },
 });
 
