d8c54079e6418362a8a37691fee5b8c676fac550
clouderMark <charnysheuski@yandex.by>
2023-11-21 15:39:10 +0300

    feat: use border color in depends of color mode

--- src/components/weatherForTheDay/WeatherForTheDay.tsx
+++ src/components/weatherForTheDay/WeatherForTheDay.tsx
@@ -1,21 +1,26 @@
 import {StyleSheet, View, FlatList} from 'react-native';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 import {selectWetherData} from '../../redux/weatherApi';
 import {useAppSelector} from '../../redux/hooks';
 import WeatherForHours from './WeatherForHours';
+import {selectMode} from '../../redux/colorSchemeSlice';
 
 const WeatherForTheDay = () => {
   const {list} = useAppSelector(selectWetherData);
+  const isDarkMode = useAppSelector(selectMode);
+
+  const border = {
+    borderColor: isDarkMode ? Colors.lighter : Colors.dark,
+  };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, border]}>
       {list.length ? (
         <FlatList
           nestedScrollEnabled
           data={list}
           horizontal
-          renderItem={({item, index}) => (
-            <WeatherForHours dt={item.dt} isFirst={!index}/>
-          )}
+          renderItem={({item, index}) => <WeatherForHours dt={item.dt} isFirst={!index} />}
         />
       ) : null}
     </View>
@@ -26,7 +31,6 @@ const styles = StyleSheet.create({
   container: {
     height: 120,
     marginTop: 40,
-    borderColor: 'white',
     borderTopWidth: 0.6,
     borderBottomWidth: 0.6,
     paddingTop: 10,
